03dd2fb925f71f47710b67895758bc66f80d0335
Piyush Patel <piyushpatel2005@gmail.com>
2017-09-25 19:23:46 +0530

    Assignment 4 for jQuery code

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
       $("#carousel-button").children("span").addClass("fa-pause");
     }
   });
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal("toggle");
+  });
+  $("#loginButton").click(function () {
+    $("#loginModal").modal('toggle');
+  });
 });
